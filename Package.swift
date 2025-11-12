c189d02352eb94a32c9d8dcd577274c0f8b7a6b9
lukezhao9 <lukezhao9@gmail.com>
2025-11-12 08:06:08 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Sliders",
+    name: "ColorKit-Sliders",
     platforms: [.iOS(.v13), .macOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
