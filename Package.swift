013063ff0abb0634b4324525a8915731011984d1
lukezhao9 <lukezhao9@gmail.com>
2025-11-12 08:37:50 +0100

    Update package name

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "ColorKit-Sliders",
+    name: "ColorKitSliders",
     platforms: [.iOS(.v13), .macOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ColorKit-Sliders",
-            targets: ["ColorKit-Sliders"]),
+            name: "ColorKitSliders",
+            targets: ["ColorKitSliders"]),
     ],
     dependencies: [
         .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.1"),
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ColorKit-Sliders",
+            name: "ColorKitSliders",
             dependencies: ["CGExtender", "Shapes", "bez"]),
         .testTarget(
             name: "SlidersTests",
-            dependencies: ["ColorKit-Sliders"]),
+            dependencies: ["ColorKitSliders"]),
     ]
 )
